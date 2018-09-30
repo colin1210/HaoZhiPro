b818417ddc37f982717b8da06a8017179f7b5315
xiejun <junxie1210@126.com>
2018-09-30 10:12:27 +0800

    branch one

--- hz-admin/src/test/java/TestLength.java
+++ hz-admin/src/test/java/TestLength.java
@@ -2,5 +2,8 @@ public class TestLength {
 
     public static void main(String[] args) {
         System.out.println("test leng");
+
+
+        System.out.println("now is ont branch one ....");
     }
 }
